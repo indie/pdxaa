fcfd80708112d58947e4ef29258bd00b0d695bbf
L.S. Cook <lsc@hackeress.com>
2013-06-09 20:16:47 -0700

    meeting search function works

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -5,12 +5,21 @@ module MeetingsHelper
     current_params.map { |cp| "q[#{cp}]=#{params['q'][cp]}" }.join("&")
   end
 
-  # param_type :day_cont link_val mon stored_vals in session
+  # render_link renders the current link_val on the param_type key 
+  # with any previously-used query
+    # param_type string example ~ day_cont
+    # link_val string example ~ Monday
+    # stored_vals Hash -> string, string (strings as keys and values)
+
   def render_link(param_type, link_val, stored_vals)
     link = "/meetings?utf8=%E2%9C%93&q[#{param_type}]=#{link_val}"
     if link_val == "W" 
     	link += "&q[codes_not_cont]=Wh"
     end
+
+  # if the key value is stored in the param type, don't redundantly include it
+  # in the link 
+
     stored_vals.each do |key, value|
       if key == param_type
         next
@@ -22,10 +31,12 @@ module MeetingsHelper
     link
   end
 
+  # can be overridden with bootstrap
+
   def render_css(param_type, link_val, stored_vals)
   	stored_vals.each do |key, value|
   		if stored_vals[param_type]==link_val
-  			return "color:green"
+  			return "color:blue"
   		end 
 	end 
   end
