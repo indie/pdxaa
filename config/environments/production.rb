01c4fc72f37b3dba1381dff8f1dd5f2d31325057
L.S. Cook <lsc@hackeress.com>
2014-12-24 12:26:30 -0800

    production variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Aasj::Application.configure do
+Pdxaa::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
